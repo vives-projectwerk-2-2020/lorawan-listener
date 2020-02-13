0b0e69a2091bfa32603037cf853a125a44bee1ef
Drees Camertijn <dreescamertijn@hotmail.com>
2020-02-13 13:00:27 +0100

    install influxdb-nodejs, test code

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-let index = require('./index.js');
+// no auth
+const Influx = require('influxdb-nodejs');
+const client = new Influx('http://127.0.0.1:8086/mydb');
 
-console.log(MqttListener());
+// normal auth (user and password will be added to URL query parameters)
+const Influx = require('influxdb-nodejs');
+const client = new Influx('http://user:pwd@127.0.0.1:8086/mydb');
+
+// basic auth (will be used http basic auth)
+const Influx = require('influxdb-nodejs');
+const client = new Influx('http://user:pwd@127.0.0.1:8086/mydb?auth=basic'
+
+// i --> integer
+// s --> string
+// f --> float
+// b --> boolean
+const fieldSchema = {
+  use: 'i',
+  bytes: 'i',
+  url: 's',
+};
+const tagSchema = {
+  spdy: ['speedy', 'fast', 'slow'],
+  method: '*',
+  // http stats code: 10x, 20x, 30x, 40x, 50x
+  type: ['1', '2', '3', '4', '5'],
+};
+client.schema('http', fieldSchema, tagSchema, {
+  // default is false
+  stripUnknown: true,
+});
+client.write('http')
+  .tag({
+    spdy: 'fast',
+    method: 'GET',
+    type: '2',  
+  })
+  .field({
+    use: 300,
+    bytes: 2312,
+    url: 'https://github.com/vicanso/influxdb-nodejs',
+  })
+  .then(() => console.info('write point success'))
+  .catch(console.error);
