8be718e0d771290108b3c00389cfd1acfb6fc9cf
Geylian lingier <geylian.l@gmail.com>
2020-02-17 16:50:29 +0100

    sending received data to influx db successfull

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-let ttn = require("ttn");
+const ttn = require("ttn");
+const Influx = require('influx');
 
 require('dotenv').config();
+
 const appID = process.env.APP_ID;
 const accessKey = process.env.ACCESSKEY;
+const influxIp = process.env.INFLUX_IP;
+const influxPort = process.env.INFLUX_PORT;
+
+const influx = new Influx.InfluxDB(`http://${influxIp}:${influxPort}/database`);
 
 // discover handler and open mqtt connection
 ttn.data(appID, accessKey)
@@ -10,9 +16,32 @@ ttn.data(appID, accessKey)
     client.on("uplink", function (devID, payload) {
       console.log("Received uplink from ", devID);
       console.log(payload);
-    })
+
+      influx.writePoints([
+        {
+          measurement: 'sensors',
+          tags: {
+            sensor_id: payload.payload_fields.device_id,
+            location: "lab2.80",
+          },
+          fields: { temperature: payload.payload_fields.temperature,
+            humidity: payload.payload_fields.humidity,
+            pm10: payload.payload_fields.pm10,
+            pm25: payload.payload_fields.pm25
+           },
+          timestamp: Math.round((new Date()).getTime() / 1000),
+        }
+      ], {
+        database: 'particulaInfluxDB',
+        precision: 's',
+      })
+      .catch(error => {
+        console.error(`Error saving data to InfluxDB! ${error.stack}`);
+      });
+      console.log("send to influx db success");
+    });
   })
   .catch(function (err) {
-    console.error(err);
+    console.error(`Error communication The Things Network API! ${err}`);
     process.exit(1);
   })
